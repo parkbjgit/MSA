17a1a559aae4245317df6c67be7a2650aca3e8a9
parkbjgit <92544688+parkbjgit@users.noreply.github.com>
2024-08-24 18:06:27 +0900

    로그인 화면 카카오, 구글 로그인으로 변경 일단 구글 로그인만 개발 예정

--- app/src/main/java/com/example/msa/LoginActivity.java
+++ app/src/main/java/com/example/msa/LoginActivity.java
@@ -9,30 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button btn_login, btn_signup;
+    private Button btn_google_login, btn_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btn_login = findViewById(R.id.btn_login);
-        btn_signup = findViewById(R.id.btn_signup);
+        btn_google_login = findViewById(R.id.btn_google_login);
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        btn_google_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // btn_login 클릭 시 activity_main으로 화면 이동
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // btn_signup 클릭 시 activity_signup으로 화면 이동
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(intent);
             }
         });
