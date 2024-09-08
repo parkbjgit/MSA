73b5a6c8827e523b9ead014198763bb269b75df2
parkbjgit <92544688+parkbjgit@users.noreply.github.com>
2024-09-08 23:52:51 +0900

    에약버튼 수정 및 취소 버튼 추가, 레이아웃 변경

--- app/src/main/java/com/example/msa/InfoFragment2.java
+++ app/src/main/java/com/example/msa/InfoFragment2.java
@@ -1,4 +1,14 @@
 package com.example.msa;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 public class InfoFragment2 {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.facility_time_select, container, false);
+
+        return view;
+    }
 }
