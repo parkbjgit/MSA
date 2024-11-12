35b2a6600eb23e42c836276f762b529e92f539a4
parkbjgit <92544688+parkbjgit@users.noreply.github.com>
2024-11-13 01:55:33 +0900

    예약 취소 api 완료

---
+++ app/src/main/java/com/example/msa/CancelRequest.java
@@ -0,0 +1,9 @@
+package com.example.msa;
+
+public class CancelRequest {
+    private String reason;
+
+    public CancelRequest(String reason) {
+        this.reason = reason;
+    }
+}
--- app/src/main/java/com/example/msa/ReservationsApiService.java
+++ app/src/main/java/com/example/msa/ReservationsApiService.java
@@ -4,11 +4,19 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ReservationsApiService {
     @POST("api/reserve/{ride_id}")
     Call<Void> reserveRide(
             @Path("ride_id") String rideId,
             @Body ReservationRequest request
+    );
+
+    @POST("api/cancel/{ride_id}")
+    Call<Void> cancelRide(
+            @Path("ride_id") String rideId,
+            @Query("user_id") String userId,
+            @Body CancelRequest reason
     );
 }
